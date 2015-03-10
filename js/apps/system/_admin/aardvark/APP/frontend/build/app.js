74abe0d52f89579a87d0e84f2c0a1f33e401fe06
Alan Plum <me@pluma.io>
2015-03-10 17:15:22 +0100

    Logs should checkUser, logout shouldn't have an href.

--- js/apps/system/_admin/aardvark/APP/frontend/build/app.js
+++ js/apps/system/_admin/aardvark/APP/frontend/build/app.js
@@ -109083,35 +109083,6 @@ window.ArangoUsers = Backbone.Collection.extend({
       "logs": "logs"
     },
 
-    logs: function () {
-      if (!this.logsView) {
-        var newLogsAllCollection = new window.ArangoLogs(
-          {upto: true, loglevel: 4}
-        ),
-        newLogsDebugCollection = new window.ArangoLogs(
-          {loglevel: 4}
-        ),
-        newLogsInfoCollection = new window.ArangoLogs(
-          {loglevel: 3}
-        ),
-        newLogsWarningCollection = new window.ArangoLogs(
-          {loglevel: 2}
-        ),
-        newLogsErrorCollection = new window.ArangoLogs(
-          {loglevel: 1}
-        );
-        this.logsView = new window.LogsView({
-          logall: newLogsAllCollection,
-          logdebug: newLogsDebugCollection,
-          loginfo: newLogsInfoCollection,
-          logwarning: newLogsWarningCollection,
-          logerror: newLogsErrorCollection
-        });
-      }
-      this.logsView.render();
-      this.naviView.selectMenuItem('tools-menu');
-    },
-
     initialize: function () {
       // This should be the only global object
       window.modalView = new window.ModalView();
@@ -109165,6 +109136,36 @@ window.ArangoUsers = Backbone.Collection.extend({
         return false;
       }
       return true;
+    },
+
+    logs: function () {
+      if (!this.checkUser()) return;
+      if (!this.logsView) {
+        var newLogsAllCollection = new window.ArangoLogs(
+          {upto: true, loglevel: 4}
+        ),
+        newLogsDebugCollection = new window.ArangoLogs(
+          {loglevel: 4}
+        ),
+        newLogsInfoCollection = new window.ArangoLogs(
+          {loglevel: 3}
+        ),
+        newLogsWarningCollection = new window.ArangoLogs(
+          {loglevel: 2}
+        ),
+        newLogsErrorCollection = new window.ArangoLogs(
+          {loglevel: 1}
+        );
+        this.logsView = new window.LogsView({
+          logall: newLogsAllCollection,
+          logdebug: newLogsDebugCollection,
+          loginfo: newLogsInfoCollection,
+          logwarning: newLogsWarningCollection,
+          logerror: newLogsErrorCollection
+        });
+      }
+      this.logsView.render();
+      this.naviView.selectMenuItem('tools-menu');
     },
 
     applicationDetail: function (mount) {
