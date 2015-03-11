03a29a6b8d2ce793aa9dbad1eddff55dadf07007
Alan Plum <me@pluma.io>
2015-03-11 12:00:46 +0100

    Readability.

--- js/server/modules/org/arangodb/foxx/manager.js
+++ js/server/modules/org/arangodb/foxx/manager.js
@@ -221,7 +221,7 @@
       "exports":            [ false, ["object", "string"] ]
     };
 
-    var att, failed = false;
+    var att, valid, failed = false;
     var expectedType, actualType;
 
     for (att in expected) {
@@ -231,11 +231,15 @@
           expectedType = expected[att][1];
           actualType = Array.isArray(mf[att]) ? "array" : typeof(mf[att]);
 
-          function matchesType(type) {
-            return actualType === type;
+          if (!Array.isArray(expectedType)) {
+            valid = (actualType === expectedType);
+          } else {
+            valid = expectedType.some(function (type) {
+              return actualType === type;
+            });
           }
 
-          if (!(Array.isArray(expectedType) ? expectedType.some(matchesType) : actualType === expectedType)) {
+          if (!valid) {
             console.error("Manifest '%s' uses an invalid data type (%s) for %s attribute '%s'",
               filename,
               actualType,
